37106b1e44e9d37a6796d890c4c8fd2f45dab63b
Wali <walisylla.esse@gmail.com>
2024-12-09 11:32:08 +0100

    test erreur

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,14 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <Header className={styles.header}>
+    <header className={styles.header}>
       <div className={styles.logoAndSearchContainer}>
         <Image className={styles.logo}>
           <Link href="/" />
         </Image>
-        <input className={searchText} placeholder="search..."></input>
+        <input className={styles.searchText} placeholder="search..."></input>
         <input
-          className={searchLocation}
+          className={styles.searchLocation}
           placeholder="select the location..."
         ></input>
         <FontAwesomeIcon
@@ -20,10 +20,10 @@ function Header() {
         />
       </div>
       <div className={styles.signinSignupContainer}>
-        <button className={btnSignin}>Sign-in</button>
-        <button className={btnSignup}>Sign-up</button>
+        <button className={styles.btnSignin}>Sign-in</button>
+        <button className={styles.btnSignup}>Sign-up</button>
       </div>
-    </Header>
+    </header>
   );
 }
 
