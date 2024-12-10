ccc336dc763ad50b2f7592855ce716d1f8301ea4
Mehmet Aladagli <mehmet.aladagli@gmail.com>
2024-12-10 17:35:12 +0100

    association page

---
+++ pages/association.js
@@ -0,0 +1,17 @@
+import Association from '../components/Association';
+import styles from '../styles/Association.module.css';
+import Link from 'next/link';
+import Header from '../components/Header';
+
+
+function AssociationPage() {
+  return ( 
+    <>
+
+      <Association />
+   
+    </>
+  );
+}
+
+export default AssociationPage;
