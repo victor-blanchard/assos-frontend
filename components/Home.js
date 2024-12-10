4151eea06249dc60d25084d17c53eb217f5a1622
Wali <walisylla.esse@gmail.com>
2024-12-10 10:52:14 +0100

    creation des composant signin et signup

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,22 @@
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 import { useState } from 'react';
-
+import { Modal, Button } from 'antd';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFutbol, faHandshakeAngle, faHeartPulse, faChildren, faDog, faPalette, faUserGraduate } from "@fortawesome/free-solid-svg-icons";
 
 
+
 function Home() {
   const [icon, setIcon] = useState(''); //Récupere le nom de l'evenement au clic sur l'icon;
-  console.log('icon =>',icon);
-
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isAsso, setIsAsso] = useState(false);// Permet de vérifier si c'est une asso
+  const [isUser, setIsUSer] = useState(false);// Permet de vérifier si c'est un particulier
+  console.log('C\'est une association =>', isAsso);
+  console.log('C\'est un particulier =>', isUser);
+  
+  
+//Tableau d'objets qui contient le nom d'un evenement associé à une icon
   const dataEvent = [ 
     {event: 'Aide à la personne', icon: faHandshakeAngle},
     {event: 'Sport', icon: faFutbol},
@@ -20,6 +27,20 @@ function Home() {
     {event: 'Education', icon: faUserGraduate},
     {event: 'Anaimaux', icon: faDog}
   ];
+
+  //Ouvre la modal 
+  const handleSign = () => {
+    setIsModalVisible(true)
+  }
+
+  // const handleSignOk = () => {
+  //   setIsModalVisible(false)
+  // }
+
+//Ferme la modal
+  const handleSignCancel = () => {
+    setIsModalVisible(false)
+  }
   
   /**
    * Permet de récuperer via le setter d'etat setIcon les données du tableau dataEVent
@@ -28,10 +49,9 @@ function Home() {
   const handleIconEVent = (event) => {
     setIcon(event);
     console.log(event);
-    // setIcon(event);
-  }
+  };
+
   const iconEvent = dataEvent.map((data, i) => { //On map sur le tableau d'objet afin de recuperer les infos de manière dynamique
-    // console.log(`Event : ${data.event}, Icon: ${data.icon}`)
     return <div className={styles.divEVent}>
         <div key={i} className={styles.cicleEvent}>
               <FontAwesomeIcon icon={data.icon}
@@ -41,7 +61,7 @@ function Home() {
           <p>{data.event}</p>
       </div>
           
-  })
+  });
 
   return (
     <div>
@@ -53,8 +73,20 @@ function Home() {
                 Explorez une large gamme d'événements, ateliers, et sorties organisés par des associations proches de vous. 
                 Que vous cherchiez à participer, apprendre, ou rencontrer, notre plateforme vous aide à trouver facilement 
                 des activités adaptées à vos envies.</p>
-            <button onClick={()=> console.log('click')} className={styles.btn}>Rejoindre La Sauce</button>
+            <Button type='primary' onClick={handleSign} className={styles.btn}>Rejoindre La Sauce</Button>
           </div>
+          <Modal
+            title="S'inscrire"
+            open={isModalVisible} // Ouvre la modal
+            onOk={handleSign} // Ferme la modal avec le bouton "OK" prévoir une redirection
+            onCancel={handleSignCancel} // Ferme la modal avec le bouton "Annuler"
+            className={styles.modal}
+          >
+            <h2>Je suis :</h2>
+            <Button>Association</Button>
+            <Button>Particulier</Button>
+            
+          </Modal>
           <div className={styles.divImg}>
             <Image src='/solidarité.jpeg'
               className={styles.imgAccueil}
