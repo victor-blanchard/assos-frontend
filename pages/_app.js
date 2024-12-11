6b66cb2322b633b2c5f43d9ecd86e6f4437f2b79
Iresha <iresha.jayakuru@gmail.com>
2024-12-11 09:58:26 +0100

    persist

--- pages/_app.js
+++ pages/_app.js
@@ -4,26 +4,47 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 //Redux
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import places from "../reducers/place";
 import users from '../reducers/users';
 
+// Redux-persist imports
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
 
 //Define the store
+// const store = configureStore({
+//   reducer: {places, users},
+//  });
+
+// Définissez le ou les reducer(s) de votre application avec la fonction combineReducers 
+const reducers = combineReducers({places, users})
+
+// Utilisez une clé de stockage pour définir un nom à votre store à l’intérieur du local storage 
+const persistConfig = { key: 'LaSauce', storage };
+
+// Mise à jour du contenu de votre store avec la fonction configureStore 
 const store = configureStore({
-  reducer: {places, users},
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
  });
 
+//  Rendre le store persistant
+ const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Header />
       <Component {...pageProps} />
       <Footer/>
+      </PersistGate>
       </Provider>
   );
 }
