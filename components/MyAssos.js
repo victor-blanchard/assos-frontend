32799e9458452a1b921270ffbdf82f2fb8444e88
Mehmet Aladagli <mehmet.aladagli@gmail.com>
2024-12-18 18:46:50 +0100

    my events & my asso

--- components/MyAssos.js
+++ components/MyAssos.js
@@ -1,127 +1,81 @@
-import React from 'react';
+import React from "react";
 import styles from "../styles/MyAssos.module.css";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-import Image from 'next/image';
+import Image from "next/image";
 import { useRouter } from "next/router";
-import { useSelector } from 'react-redux';
-
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
 function MyAssos() {
-    const router = useRouter();
+  const router = useRouter();
+  const user = useSelector((state) => state.users.value);
+  const token = user.token;
+  console.log("token of user in Assos pasge ==> " + token);
 
-    // Accéder aux assos favories depuis le store Redux
-    const followingAssociations = useSelector((state) => state.users.value.followingAssociations);
-<<<<<<< HEAD
-    console.log("followed assoss===>" + followingAssociations)
+  const [likedAssos, setLikedAssos] = useState([]);
 
-=======
-console.log(followingAssociations)
->>>>>>> 1c53b3babe47097f3f9ec0079244bc775c823ad4
-    // Fonction pour rediriger à la page d'une asso
-    const handleClick = (assoId) => {
-        router.push(`/association/${assoId}`);
-    };
+  useEffect(() => {
+    if (user && user.token) {
+      fetch(`http://localhost:3000/users/followingAssociations/${user.token}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("API response:", data);
+          setLikedAssos(data.followingAssociations); // likedAssos dizisini al
+        })
+        .catch((error) => console.error("API fetch error:", error));
+    }
+  }, [user]);
 
+  const handleClick = (eventId) => {
+    router.push(`/event/${eventId}`);
+  };
 
-    const handleAssociationDisplay = (id) => {
-        router.push(`/public_association?id=${id}`);
-    };
+  return (
+    <div className={styles.main}>
+      <div className={styles.leftSection}>
 
-    const associations = [
-        {
-            name: "Association A",
-            image: "https://via.placeholdercom/150",
-        },
-        {
-            name: "Association B",
-            image: "https://via.placeholder.com/150",
-        },
-        {
-            name: "Association C",
-            image: "https://via.placeholder.com/150",
-        },
-        {
-            name: "Association D",
-            image: "https://via.placeholder.com/150",
-        },
-    ];
+      <Link href="/" className={styles.homeLink}>
+        <FontAwesomeIcon icon={faArrowLeft} /> Retour A la page d'accueil
+      </Link>
+      </div>
 
-
-
-    return (
-        <div className={styles.main}>
-<<<<<<< HEAD
-            
-            <div className={styles.leftSection}>
-                <Link href="/userProfil" className={styles.homeLink}>Retour à mon profil</Link>
-                <Link href="/" className={styles.homeLink}>Retour à la page d'accueil</Link>
-=======
-            {/* Section gauche */}
-            <div className={styles.leftSection}>
-                <Link href="/" className={styles.homeLink}>
-                    <FontAwesomeIcon icon={faArrowLeft} /> Retour à la page d'accueil
-                </Link>
->>>>>>> 1c53b3babe47097f3f9ec0079244bc775c823ad4
-            </div>
-
-            {/* Section droite */}
-            <div className={styles.rightSection}>
-                <h1 className={styles.title}>Mes Associations</h1>
-                <div className={styles.container}>
-                    {/* {followingAssociations && followingAssociations.length > 0 ? (
-                        followingAssociations.map((associations) => (
-                            <div
-                                key={associations.id}
-                                className={styles.assoBox}
-                                onClick={() => handleClick(associations.id)}
-                            
-                            > */}
-
-                    {followingAssociations && followingAssociations.length > 0 ? (
-                        followingAssociations.map((association) => (
-                            // <div
-                            //     key={association._id || index} // Utilisez un ID unique
-                            //     onClick={() => handleClick(association._id)}
-                            //     className={styles.card}
-                            // >
-                                // <div
-                                //     onClick={() => handleAssociationDisplay(data._id)}
-                                //     id={data._id}
-                                //     key={i}
-                                //     className={styles.card}
-                                // >
-                                    <Image
-                                        src={association.image || '/asso.jpg'} // Image par défaut si aucune image n'est fournie
-                                        alt={`Image de ${association.name}`}
-                                        className={styles.myAssosImage}
-                                        width={200}
-                                        height={150}
-                                    />
-
-                                    {/* 
-                    {associations.map((public_association, index) => (
-
-                        <div key={index} className={styles.associationBox} onClick={() => handleClick(public_association)}>
-
-                            <Image
-                                src='/asso.jpg'
-                                alt="image d'association"
-                                className={styles.myAssosImage}
-                                width={200}
-                                height={150}
-                            /> */}
-
-                                    <h3 className={styles.myAssosTitle}>{association.name}</h3>
-                                </div>
-                                ))
-                                ) : (
-                                <p>Aucune association trouvée.</p> // Message par défaut
-                )}
-                            </div>
+      <div className={styles.rightSection}>
+        <h1 className={styles.title}>Mes associations</h1>
+        <div className={styles.container}>
+          {likedAssos.length > 0 ? (
+            likedAssos.map((asso) => (
+              <div
+                key={asso._id}
+                className={styles.eventBox}
+                onClick={() => handleClick(asso._id)}
+              >
+                <Image
+                  src={asso.image}
+                  alt={asso.title}
+                  className={styles.eventImage}
+                  width={100}
+                  height={100}
+                />
+                <div className={styles.eventDetails}>
+                  <h2 className={styles.eventTitle}>{asso.name}</h2>
+                  <p className={styles.eventDescription}>{asso.description}</p>
+                  <p className={styles.eventDescription}>{asso.categories.join(', ')}</p>
+                  <p className={styles.eventDate}>Rue: {asso.address.street}</p>
+                  <p className={styles.eventDate}>Ville: {asso.address.city}</p>
+                  <p className={styles.eventDate}>Code Postale: {asso.address.zipcode}</p>
+                  <p className={styles.eventDate}>E-mail: {asso.email}</p>
+                  <p className={styles.eventDate}>Téléphone: {asso.phone}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>Aucun évenement dans cette catégorie.</p>
+          )}
         </div>
-            </div>
-            );
+      </div>
+    </div>
+  );
 }
-            export default MyAssos;
+export default MyAssos;
