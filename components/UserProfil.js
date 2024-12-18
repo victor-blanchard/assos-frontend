5eb61fffbc4a799b163b27150ee3947848320d95
Iresha <iresha.jayakuru@gmail.com>
2024-12-18 14:48:37 +0100

    merge conflict resolve

--- components/UserProfil.js
+++ components/UserProfil.js
@@ -4,45 +4,36 @@ import { useSelector, useDispatch } from 'react-redux';
 import AssociationCard from './AssociationCards';
 import { useRouter } from "next/router";
 
+
 function UserProfil() {
   const router = useRouter();
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.users.value);
-  const associations = useSelector((state) => state.associations.value);
-  const token = user.token;
-  const titleCards = [
-    {
-      title: "Mes associations",
-      description: "Retrouvez toutes les associations qui vous interessent.",
-      route: "/my_assos",
-    },
-    {
-      title: "Mes evenements à venir",
-      description: "Les evenementd à venir de vos associations préferées au même endroit.",
-      route: "/my_events",
-    },
-    {
-      title: "Mes notifications",
-      description: "Vos événements programmés.",
-      route: "/notification",
-    },
-    // {title: 'Mes notifications', description: 'Vos événements programmés.'},
-  ];
-  console.log("User session =>", token);
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.users.value);
+    const associations = useSelector((state) => state.associations.value);
+    const token = user.token;
+    const titleCards = [
+        {title: 'Mes associations', description: 'Retrouvez toutes les associations qui vous interessent.', route: '/my_assos'}, 
+        {title: 'Mes evenements à venir', description: 'Les evenementd à venir de vos associations préferées au même endroit.', route:'/my_events'}, 
+        {title: 'Mes notifications', description: 'Vos événements programmés.', route:'/notification'}, 
+        // {title: 'Mes notifications', description: 'Vos événements programmés.'},  
+    ];
+    console.log('User session =>', token);
 
-  const handleCardClick = (route) => {
-    // console.log('click', link);
-    if (route) {
-      router.push(route); // Redirection vers l'URL spécifiée
-    }
+    const handleCardClick = (route) => {
+      // console.log('click', link);
+      if (route) {
+          router.push(route); // Redirection vers l'URL spécifiée
+      }
+      
   };
 
-  const cards = titleCards.map((data, i) => (
-    <AssociationCard
-      key={i}
-      title={data.title}
-      description={data.description || "Description non disponible."}
-      onClick={() => handleCardClick(data.route)} // Passez le lien à la fonction onClick
+const cards = titleCards.map((data, i) => (
+  <AssociationCard 
+        key={i} 
+        title={data.title} 
+        description={data.description || 'Description non disponible.'} 
+        onClick={() => handleCardClick(data.route)} // Passez le lien à la fonction onClick
+        
     />
   
 
@@ -51,7 +42,7 @@ function UserProfil() {
 
     
     const userProfil = (
-        <div>
+        <div className={styles.leftContainer}>
             <div className={styles.userImgProfil}>
                 <Image  
                     src="/user_profil.jpg"
@@ -64,6 +55,11 @@ function UserProfil() {
             <div>
                 <h2>Profil de {user.username}</h2>
                 <p>Adresse mail: {user.email}</p>
+                <div className={styles.memberInfo}>
+                  <p>Groupes</p>
+                  <p>Centres d'intérêt</p>
+                  <p>Inscriptions</p>
+                </div>
             </div>
         </div>
         )
@@ -75,7 +71,11 @@ function UserProfil() {
       <section className={styles.section}>
         <div className={styles.sectionLeftSIde}>{userProfil}</div>
         <div className={styles.sectionRightSide}>
-          {cards.length > 0 ? cards : <p>Aucune donnée disponible pour le moment.</p>}
+          {cards.length > 0 ? (
+            cards
+          ) : (
+            <p>Aucune donnée disponible pour le moment.</p>
+          )}
         </div>
       </section>
       {/* {showModal && modalContent} Show modal */}
